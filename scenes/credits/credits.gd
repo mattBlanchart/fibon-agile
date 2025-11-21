56f14719d8a26077b32b50156c1345f55b8aa0ec
Rekanoo <florian.paillebarrere@gmail.com>
2025-11-21 09:08:46 +0100

    Adding delta time for speed

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -4,6 +4,7 @@ var isGoingToRight: bool = true;
 var isGoingToLeft: bool = false;
 var isGoingDown: bool = false;
 var isGoingUp: bool = false;
+var speed: int = 200;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
@@ -13,27 +14,27 @@ func _ready():
 	
 func _process(delta: float) -> void:
 
-	if $CanvasLayer/FloBody2D.position.x == 120 && $CanvasLayer/FloBody2D.position.y == 0:
+	if $CanvasLayer/FloBody2D.position.x >= 120 && $CanvasLayer/FloBody2D.position.y <= 0:
 		isGoingToRight = false
 		isGoingDown = true
-	elif $CanvasLayer/FloBody2D.position.x == 120 && $CanvasLayer/FloBody2D.position.y == 200:
+	elif $CanvasLayer/FloBody2D.position.x >= 120 && $CanvasLayer/FloBody2D.position.y >= 200:
 		isGoingDown=false
 		isGoingToLeft=true
-	elif $CanvasLayer/FloBody2D.position.x == 0 && $CanvasLayer/FloBody2D.position.y == 200:
+	elif $CanvasLayer/FloBody2D.position.x <= 0 && $CanvasLayer/FloBody2D.position.y >= 200:
 		isGoingToLeft=false
 		isGoingUp=true
-	elif $CanvasLayer/FloBody2D.position.x == 0 && $CanvasLayer/FloBody2D.position.y == 0:
+	elif $CanvasLayer/FloBody2D.position.x <= 0 && $CanvasLayer/FloBody2D.position.y <= 0:
 		isGoingToRight=true
 		isGoingUp=false
 		
 	if isGoingToRight:
-		$CanvasLayer/FloBody2D.position.x += 5	
+		$CanvasLayer/FloBody2D.position.x += speed	* delta
 	elif isGoingToLeft:
-		$CanvasLayer/FloBody2D.position.x -= 5	
+		$CanvasLayer/FloBody2D.position.x -= speed	* delta
 	elif isGoingDown:
-		$CanvasLayer/FloBody2D.position.y += 5
+		$CanvasLayer/FloBody2D.position.y += speed	* delta
 	elif isGoingUp:
-		$CanvasLayer/FloBody2D.position.y -= 5
+		$CanvasLayer/FloBody2D.position.y -= speed	* delta
 	
 	print("x = " + str($CanvasLayer/FloBody2D.position.x) + " y = " + str($CanvasLayer/FloBody2D.position.y) + " right = " + str(isGoingToRight) + " left = " + str(isGoingToLeft)
 		+ " up = " + str(isGoingUp) + " down = " + str(isGoingDown))
