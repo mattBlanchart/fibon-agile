a57dfcb7fa6c9805bef7488e0416e581d9d627dd
Rekanoo <florian.paillebarrere@gmail.com>
2025-11-21 08:43:06 +0100

    Add flo credits

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -1,9 +1,42 @@
 extends Control #pour la navigation
 
+var isGoingToRight: bool = true;
+var isGoingToLeft: bool = false;
+var isGoingDown: bool = false;
+var isGoingUp: bool = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
 	pass
+
+
+	
+func _process(delta: float) -> void:
+
+	if $CanvasLayer/FloBody2D.position.x == 120 && $CanvasLayer/FloBody2D.position.y == 0:
+		isGoingToRight = false
+		isGoingDown = true
+	elif $CanvasLayer/FloBody2D.position.x == 120 && $CanvasLayer/FloBody2D.position.y == 200:
+		isGoingDown=false
+		isGoingToLeft=true
+	elif $CanvasLayer/FloBody2D.position.x == 0 && $CanvasLayer/FloBody2D.position.y == 200:
+		isGoingToLeft=false
+		isGoingUp=true
+	elif $CanvasLayer/FloBody2D.position.x == 0 && $CanvasLayer/FloBody2D.position.y == 0:
+		isGoingToRight=true
+		isGoingUp=false
+		
+	if isGoingToRight:
+		$CanvasLayer/FloBody2D.position.x += 5	
+	elif isGoingToLeft:
+		$CanvasLayer/FloBody2D.position.x -= 5	
+	elif isGoingDown:
+		$CanvasLayer/FloBody2D.position.y += 5
+	elif isGoingUp:
+		$CanvasLayer/FloBody2D.position.y -= 5
+	
+	print("x = " + str($CanvasLayer/FloBody2D.position.x) + " y = " + str($CanvasLayer/FloBody2D.position.y) + " right = " + str(isGoingToRight) + " left = " + str(isGoingToLeft)
+		+ " up = " + str(isGoingUp) + " down = " + str(isGoingDown))
 	
 func _on_quit_credit_pressed() -> void:
 		print("Go to Welcome page")
