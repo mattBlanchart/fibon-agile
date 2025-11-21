537ad9bcd32817d1587be6363dfb5a8e347a8eca
Rekanoo <florian.paillebarrere@gmail.com>
2025-11-21 07:53:18 +0100

    Ajout retro scene

--- scenes/welcome/welcome.gd
+++ scenes/welcome/welcome.gd
@@ -3,6 +3,7 @@ extends Control
 # variables pour initialiser les scénes à changer
 @export var main_scene: PackedScene
 @export var credits_scene: PackedScene
+@export var retrospective_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,5 +21,13 @@ func _on_start_button_credits_pressed() -> void:
 	if credits_scene:
 		print("Go to credits scene")
 		get_tree().change_scene_to_packed(credits_scene)
+	else:
+		print("un truc")
+		
+# pour aller à la rétrospective
+func _on_start_button_retrospective_pressed() -> void:
+	if retrospective_scene:
+		print("Go to retrospective scene")
+		get_tree().change_scene_to_packed(retrospective_scene)
 	else:
 		print("un truc")
