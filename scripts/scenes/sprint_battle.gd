c0786e6c2509f4a75a5a8662075eab569a199470
Rekanoo <florian.paillebarrere@gmail.com>
2025-12-12 21:23:44 +0100

    Equilibrage hp sprint et dwc bug

--- scripts/scenes/sprint_battle.gd
+++ scripts/scenes/sprint_battle.gd
@@ -49,7 +49,7 @@ func _ready() -> void:
 	
 	# Ici, utiliser GameState.pi et GameState.sprint pour le sprint et le pi courant
 	# Mettre en place la formule pour la difficulter
-	sprint_hp_max = ((GameState.pi + GameState.sprint)*5 -5)*(GameState.pi + GameState.sprint)/Database.pi_size
+	sprint_hp_max = ((GameState.pi + GameState.sprint)*5 -5)*(GameState.pi + GameState.sprint)/Database.pi_size - 1
 	sprint_hp_max += GameState.bugs
 	GameState.bugs = 0
 	sprint_hp = sprint_hp_max
@@ -58,7 +58,7 @@ func _ready() -> void:
 	
 	deck = PlayerState.get_current_deck()
 	
-	_draw_cards(5)
+	_draw_cards_5()
 	
 	for unit in PlayerState.units:		
 		var sprite = TextureRect.new()
